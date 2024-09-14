a67ecfeca08dcb68e535c211e8f2cd8fbaa3f3de
Steven Chen <2381824724@qq.com>
2024-09-13 20:54:43 -0400

    22nd commit

--- src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
+++ src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
@@ -1,31 +1,36 @@
 package com.stevenchen.redisplugin.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.data.redis.core.RedisTemplate;
-import run.halo.app.event.comment.CommentNewEvent;  // 使用通用事件类
-import run.halo.app.event.post.PostCreatedEvent;   // 使用通用事件类
+import org.springframework.context.ApplicationEvent;  // 使用通用事件类
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class HaloEventListener {
+public class HaloEventListener implements ApplicationListener<ApplicationEvent> {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     private final String STREAM_KEY = "halo-stream";
 
-    @EventListener
-    public void onPostCreated(PostCreatedEvent event) {
-        publishMessage("POST_CREATED", event.getPostId());  // 使用现有方法获取文章ID
-    }
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        // 打印事件类型，帮助确认触发的事件
+        System.out.println("Received event: " + event.getClass().getSimpleName());
 
-    @EventListener
-    public void onCommentAdded(CommentNewEvent event) {
-        publishMessage("COMMENT_ADDED", event.getCommentId());  // 使用现有方法获取评论ID
+        // 检查事件的类型，并根据不同的事件类型执行操作
+        if (event instanceof PostCreatedEvent) {
+            PostCreatedEvent postEvent = (PostCreatedEvent) event;
+            publishMessage("POST_CREATED", postEvent.getPostId());
+        } else if (event instanceof CommentNewEvent) {
+            CommentNewEvent commentEvent = (CommentNewEvent) event;
+            publishMessage("COMMENT_ADDED", commentEvent.getCommentId());
+        }
     }
 
     private void publishMessage(String action, Long id) {
