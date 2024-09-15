2e1ea43e070ffd4bc5c369eeb71268bff418c455
Steven Chen <2381824724@qq.com>
2024-09-15 12:50:27 -0400

    35th - add log

--- src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
+++ src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
@@ -1,5 +1,7 @@
 package com.stevenchen.redisplugin.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
@@ -12,6 +14,8 @@ import java.util.Map;
 @Component
 public class HaloEventListener implements ApplicationListener<ApplicationEvent> {
 
+    private static final Logger logger = LoggerFactory.getLogger(HaloEventListener.class);
+
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
@@ -19,18 +23,26 @@ public class HaloEventListener implements ApplicationListener<ApplicationEvent> 
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        // 打印事件的类名，便于调试确认事件类型
-        System.out.println("Received event: " + event.getClass().getName());
+        // 记录事件的类名，便于调试确认事件类型
+        logger.info("Received event: " + event.getClass().getName());
 
         // 根据事件的实际类型进行处理
         if (event.getClass().getSimpleName().equals("PostCreatedEvent")) {
-            // 假设事件有 getPostId() 方法
             Long postId = getPostIdFromEvent(event);
-            publishMessage("POST_CREATED", postId);
+            if (postId != null) {
+                logger.info("PostCreatedEvent detected, postId: " + postId);
+                publishMessage("POST_CREATED", postId);
+            } else {
+                logger.warn("PostCreatedEvent detected, but postId is null.");
+            }
         } else if (event.getClass().getSimpleName().equals("CommentNewEvent")) {
-            // 假设事件有 getCommentId() 方法
             Long commentId = getCommentIdFromEvent(event);
-            publishMessage("COMMENT_ADDED", commentId);
+            if (commentId != null) {
+                logger.info("CommentNewEvent detected, commentId: " + commentId);
+                publishMessage("COMMENT_ADDED", commentId);
+            } else {
+                logger.warn("CommentNewEvent detected, but commentId is null.");
+            }
         }
     }
 
@@ -40,7 +52,7 @@ public class HaloEventListener implements ApplicationListener<ApplicationEvent> 
         message.put("id", id.toString());
 
         // 添加调试日志，确保方法被调用
-        System.out.println("Publishing message to Redis: action=" + action + ", id=" + id);
+        logger.info("Publishing message to Redis: action=" + action + ", id=" + id);
 
         redisTemplate.opsForStream().add(STREAM_KEY, message);
     }
@@ -49,7 +61,7 @@ public class HaloEventListener implements ApplicationListener<ApplicationEvent> 
         try {
             return (Long) event.getClass().getMethod("getPostId").invoke(event);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error getting postId from event", e);
             return null;
         }
     }
@@ -58,7 +70,7 @@ public class HaloEventListener implements ApplicationListener<ApplicationEvent> 
         try {
             return (Long) event.getClass().getMethod("getCommentId").invoke(event);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error getting commentId from event", e);
             return null;
         }
     }
