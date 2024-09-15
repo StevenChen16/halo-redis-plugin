d2cb9d871189f21d6dd3c842045ee8f2a240cfc5
Steven Chen <2381824724@qq.com>
2024-09-15 19:29:19 -0400

    55th commit

--- src/main/java/run/halo/redisplugin/config/WebConfig.java
+++ src/main/java/run/halo/redisplugin/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.stevenchen.redisplugin.config;
 
 import com.stevenchen.redisplugin.listener.HaloEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,8 +12,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Configuration(proxyBeanMethods = false)  // 添加 proxyBeanMethods = false
+@Configuration(proxyBeanMethods = false)
 public class WebConfig implements WebMvcConfigurer {
+
+    // 添加logger
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
     @Autowired
     private HaloEventListener haloEventListener;
@@ -20,12 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new HandlerInterceptor() {
 
+            @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
                 String requestURI = request.getRequestURI();
                 String method = request.getMethod();
-            
-                logger.info("拦截到请求: " + requestURI + " 方法: " + method);  // 新增日志
-            
+
+                // 添加日志记录
+                logger.info("拦截到请求: " + requestURI + " 方法: " + method);  
+
                 Long id = extractIdFromRequest(request);
                 if (requestURI.contains("/posts") && method.equals("PUT")) {
                     if (requestURI.contains("/publish")) {
@@ -34,17 +41,15 @@ public class WebConfig implements WebMvcConfigurer {
                         haloEventListener.handleRequest("POST_UPDATED", id);
                     }
                 } else if (requestURI.contains("/posts") && method.equals("DELETE")) {
-                    logger.info("拦截到文章删除请求: " + id);  // 新增日志
+                    logger.info("拦截到文章删除请求: " + id);  
                     haloEventListener.handleRequest("POST_DELETED", id);
                 } else if (requestURI.contains("/comments") && method.equals("POST")) {
                     haloEventListener.handleRequest("COMMENT_ADDED", id);
                 } else if (requestURI.contains("/comments") && method.equals("DELETE")) {
                     haloEventListener.handleRequest("COMMENT_DELETED", id);
                 }
-            
                 return true;
             }
-            
 
             private Long extractIdFromRequest(HttpServletRequest request) {
                 String[] uriParts = request.getRequestURI().split("/");
