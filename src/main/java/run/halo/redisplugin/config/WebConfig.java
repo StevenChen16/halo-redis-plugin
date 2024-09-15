17aa982a12275648047da28355dd5a6ad8e94571
Steven Chen <2381824724@qq.com>
2024-09-15 19:18:35 -0400

    51st commit

--- src/main/java/run/halo/redisplugin/config/WebConfig.java
+++ src/main/java/run/halo/redisplugin/config/WebConfig.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,7 +18,37 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(haloEventListener)
-                .addPathPatterns("/apis/content.halo.run/v1alpha1/posts/**", "/apis/content.halo.run/v1alpha1/comments/**");
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                String requestURI = request.getRequestURI();
+                String method = request.getMethod();
+
+                Long id = extractIdFromRequest(request);
+                if (requestURI.contains("/posts") && method.equals("PUT")) {
+                    if (requestURI.contains("/publish")) {
+                        haloEventListener.handleRequest("POST_CREATED", id);
+                    } else {
+                        haloEventListener.handleRequest("POST_UPDATED", id);
+                    }
+                } else if (requestURI.contains("/posts") && method.equals("DELETE")) {
+                    haloEventListener.handleRequest("POST_DELETED", id);
+                } else if (requestURI.contains("/comments") && method.equals("POST")) {
+                    haloEventListener.handleRequest("COMMENT_ADDED", id);
+                } else if (requestURI.contains("/comments") && method.equals("DELETE")) {
+                    haloEventListener.handleRequest("COMMENT_DELETED", id);
+                }
+                return true;
+            }
+
+            private Long extractIdFromRequest(HttpServletRequest request) {
+                String[] uriParts = request.getRequestURI().split("/");
+                try {
+                    return Long.parseLong(uriParts[uriParts.length - 1]);
+                } catch (NumberFormatException e) {
+                    return null;
+                }
+            }
+        }).addPathPatterns("/apis/content.halo.run/v1alpha1/posts/**", "/apis/content.halo.run/v1alpha1/comments/**");
     }
 }
--- src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
+++ src/main/java/run/halo/redisplugin/listener/HaloEventListener.java
@@ -5,15 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class HaloEventListener implements HandlerInterceptor {
+public class HaloEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(HaloEventListener.class);
 
@@ -22,50 +19,8 @@ public class HaloEventListener implements HandlerInterceptor {
 
     private final String STREAM_KEY = "halo-stream";
 
-    // 拦截API请求，不使用 @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String requestURI = request.getRequestURI();
-        String method = request.getMethod();
-
-        if (requestURI.contains("/posts") && method.equals("PUT")) {
-            Long postId = extractIdFromRequest(request);
-            if (requestURI.contains("/publish")) {
-                logger.info("拦截到文章发布请求，postId: " + postId);
-                publishMessage("POST_CREATED", postId);
-            } else {
-                logger.info("拦截到文章修改请求，postId: " + postId);
-                publishMessage("POST_UPDATED", postId);
-            }
-        } else if (requestURI.contains("/posts") && method.equals("DELETE")) {
-            Long postId = extractIdFromRequest(request);
-            logger.info("拦截到文章删除请求，postId: " + postId);
-            publishMessage("POST_DELETED", postId);
-        } else if (requestURI.contains("/comments") && method.equals("POST")) {
-            Long commentId = extractIdFromRequest(request);
-            logger.info("拦截到评论发布请求，commentId: " + commentId);
-            publishMessage("COMMENT_ADDED", commentId);
-        } else if (requestURI.contains("/comments") && method.equals("DELETE")) {
-            Long commentId = extractIdFromRequest(request);
-            logger.info("拦截到评论删除请求，commentId: " + commentId);
-            publishMessage("COMMENT_DELETED", commentId);
-        }
-
-        return true;
-    }
-
-    // 提取ID
-    private Long extractIdFromRequest(HttpServletRequest request) {
-        String[] uriParts = request.getRequestURI().split("/");
-        try {
-            return Long.parseLong(uriParts[uriParts.length - 1]);
-        } catch (NumberFormatException e) {
-            logger.warn("无法从请求URI中提取ID: " + request.getRequestURI());
-            return null;
-        }
-    }
-
-    // 发布消息到Redis Stream
-    private void publishMessage(String action, Long id) {
+    // 自定义的业务逻辑，可以通过WebConfig来触发
+    public void handleRequest(String action, Long id) {
         Map<String, Object> message = new HashMap<>();
         message.put("action", action);
         message.put("id", id != null ? id.toString() : "unknown");
