9767d842b4b25c5cd912d9015c8f87b0d5e912ff
Steven Chen <2381824724@qq.com>
2024-09-15 11:48:54 -0400

    32nd commit

--- src/main/java/run/halo/redisplugin/config/RedisConfig.java
+++ src/main/java/run/halo/redisplugin/config/RedisConfig.java
@@ -3,14 +3,13 @@ package com.stevenchen.redisplugin.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class RedisConfig {
 
     @Autowired
